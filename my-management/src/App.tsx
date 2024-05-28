e76e48c9ef8adf1ccc965289a8a79135a4c3b792
unknown <zaid_zhangyong@outlook.com>
2024-05-28 18:27:01 +0800

    1

--- my-management/src/App.tsx
+++ my-management/src/App.tsx
@@ -1,15 +1,14 @@
 import { useRoutes, Link } from "react-router-dom" //hook
 import Router from "./router"
-import { log } from "console"
 
 
 function App() {
   const outlet = useRoutes(Router)
-  console.log(outlet);
 
   return (
     <>
       <div className="App">
+
         {outlet}
       </div>
     </>
--- my-management/src/views/Login/index.tsx
+++ my-management/src/views/Login/index.tsx
@@ -1,6 +1,9 @@
+import Banner from "@/assets/images/banner.webp"
+import styles from "./login.module.scss"
 export default function Login() {
     return (
         <>
+            <img className={styles.banner   } src={Banner} alt="" />
             <div>登录</div>
         </>
     )
